3f4c3fd804c8378feba79e986807197b34a36bfe
Goldriel <krutobok@gmail.com>
2023-08-18 00:07:11 +0300

    Почти все работает, но почему то не работает продолжу завтра

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,55 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type Dict struct {
+	Word  string
+	Count int
+}
+
+var re = regexp.MustCompile(`\s{2,}|\t{2,}|\n+`)
+
+func Top10(str string) []string {
 	// Place your code here.
-	return nil
+	var dict = make(map[string]int)
+	var list []string
+	var endList []string
+	d := make([]Dict, 10)
+
+	if str == "" {
+		return nil
+	}
+
+	str = re.ReplaceAllString(str, " ")
+	list = strings.Split(str, " ")
+
+	for _, value := range list {
+		_, ok := dict[value]
+		if ok {
+			dict[value]++
+		} else {
+			dict[value] = 1
+		}
+	}
+
+	for word, count := range dict {
+		d = append(d, Dict{word, count})
+	}
+
+	sort.Slice(d, func(i, j int) bool {
+		return d[i].Count > d[j].Count
+	})
+
+	for i := 0; ; i++ {
+		if i == 10 || i == len(d) {
+			break
+		}
+		endList = append(endList, d[i].Word)
+	}
+
+	return endList
 }
